c43b4644a406317064b1bdcd11dbe474476cc4e7
Zarzuket <davide.bajo@gmail.com>
2021-09-01 16:51:40 +0200

    add exercise

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 // Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
 // Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
 // Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
+
+var squareUtente = parseInt(prompt("inserisci un numero da 1 a 100 per creare un campo di quadrati."));
+
+while(isNaN(squareUtente)){
+    squareUtente = parseInt(prompt("devi inseririre un numero da 1 a 100!!"));
+}
+
+function xSquare(){
+    for( var i=1; i<=squareUtente; i++){
+        // console.log(i);
+        document.getElementById("campo").innerHTML += `<div class="quadrato">${i}</div>`
+    }
+}
+
+xSquare(squareUtente);
+
+
+document.getElementById("campo").addEventListener("click" ,
+    function(event){
+        event.target.classList.add("red");
+        alert(event.target.innerHTML)
+    }
+)
